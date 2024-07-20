3cc06b9e38f0125fd473366b1b82ebd79f6d40c8
farias94 <farias94@protonmail.com>
2024-07-19 23:30:50 -0400

    s

---
+++ assets/load-design-tokens.js
@@ -0,0 +1,17 @@
+fetch('{{ "design-tokens.json" | asset_url }}')
+  .then(response => response.json())
+  .then(tokens => {
+    const root = document.documentElement;
+
+    // Loop through the tokens and set them as CSS variables
+    Object.keys(tokens.color).forEach(key => {
+      root.style.setProperty(`--${key}`, tokens.color[key].value);
+    });
+
+    Object.keys(tokens.typography).forEach(key => {
+      const typography = tokens.typography[key].value;
+      Object.keys(typography).forEach(prop => {
+        root.style.setProperty(`--${key}-${prop}`, typography[prop].value);
+      });
+    });
+  });
